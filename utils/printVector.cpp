d71370ffebf84452033fa3b4b9899d16617219d9
VictorDegliame <victor.degliame.pro@gmail.com>
2016-04-26 01:03:55 -0400

    Add: utils folder and printVector

---
+++ utils/printVector.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+void printVector(vector<T> v) {
+	for (const auto& val : v)
+		cout << val << " ";
+    cout << endl;
+}
