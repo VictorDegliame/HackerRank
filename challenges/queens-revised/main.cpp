183e15abef4f6f75d148536325806c7408444175
Victor Degliame <vdegliame@runads.com>
2016-05-14 13:57:57 +0200

    Update: Kind of finished queens revised. Add: architecture for permutation game

--- challenges/queens-revised/main.cpp
+++ challenges/queens-revised/main.cpp
@@ -1,100 +1,238 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
+#include <string>
+#include <cassert>
+#include <unordered_set>
+#include <ctime>
+#include <vector>
 #include <algorithm>
+#include <thread>
+#include <climits>
+
 using namespace std;
 
-const int N = 9;
-
-template<typename T>
-void printVector(ostream& os, const vector<T>& vec) noexcept {
-	// os << "[";
-	if (vec.size() != 0) {
-		for (auto it = begin(vec); it != end(vec) - 1; ++it) {
-			os << *it + 1 << " ";
-		}
-		os << vec.back() + 1;
+class Solver {
+  public:
+	Solver(int n) : N(n) {
+		startTime = clock();
+		buffer.reserve(N);
+		generateCoefs();
+		solve();
 	}
-	// os << "]" << endl;
-	os << endl;
-}
 
-void printBoard(vector<int>& queens) {
+	void solve(int maxn = -1) {
+		assert(maxn < NMAX && "maxn shall not be greater than anticipated");
 
-	cout << N << endl;
-	printVector(cout, queens);
+		if (maxn < 0)
+			maxn = N;
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			if (queens[j] == i)
-				cerr << "X ";
-			else
-				cerr << "- ";
-		}
-		cerr << endl;
+		initializeBoard();
+
+		search();
+		printSolution();
 	}
-	cerr << "...................................." << endl;
-}
 
-void placeQueens(int pos, vector<int>& queens) {
-	// cerr << "Evaluating queen " << pos + 1 << endl;
-	// Try to place the queen on every vertical line until you find the right one
-	for (int i = 0; i < N; ++i) {
-		// cerr << "For queen at X = " << pos << endl;
-		// Would that queen be well placed? Look if we don't break any rule with previously placed queens.
-		bool placementOk = true;
-		// For all queens placed behind me:
-		int offsetX = 1;
-		for (int j = pos - 1; j >= 0 && placementOk; --j, ++offsetX) {
-			int diffFirst2Queens = i - queens[j];
-			// check that we are not on the same line (only need to check pairs of 2 queens)
-			if (queens[j] == i || abs(diffFirst2Queens) == offsetX) {
-				// cerr << "Can't place queen at position " << pos << " " << i << endl;
-				placementOk = false;
-				break;
+  private:
+	int gcd(int a, int b) {
+		if (b != 0)
+			return gcd(b, a % b);
+
+		return a;
+	}
+
+	void printCoefs() {
+		for (int i = 1; i < NMAX; ++i) {
+			for (int j = 1; j < 2 * NMAX; ++j) {
+				if (j == NMAX)
+					continue;
+				cout << coefs[i][j] << " ";
 			}
-			// Check that there is not a third queen on the same line!
-			for (int jumpSize = 1; placementOk && jumpSize <= (pos - j); jumpSize++) {
-				int offsetY = diffFirst2Queens;
-				//  && (N - abs(offsetY)) >= 0
-				for (int k = j - jumpSize; k >= 0; k -= jumpSize, offsetY += diffFirst2Queens) {
-					// cerr << "I am trying to place queen (" << pos << ", " << i << "), examining queens " << j << " and " << k << endl;
-					// cerr << "(" << pos + 1 << ", " << i + 1 << "), " <<
-					//      "(" << j + 1 << ", " << queens[j] + 1 << "), " <<
-					//      "(" << k + 1 << ", " << queens[k] + 1 << ")" << endl;
-					// cerr << "OffsetX is: " << offsetX << " OffsetY is: " << offsetY << endl;
-					int diffLast2Queens = queens[j] - queens[k];
-					// cerr << "diffLast2Queens is: " << diffLast2Queens << endl;
-					if (diffLast2Queens == offsetY) {
-						// cerr << "(" << pos + 1 << ", " << i + 1 << ") and " <<
-						//      "(" << j + 1 << ", " << queens[j] + 1 << ") and " <<
-						//      "(" << k + 1 << ", " << queens[k] + 1 << ") are on the same line" << endl;
-						placementOk = false;
-						break;
-					}
+			cout << endl;
+		}
+	}
+
+	float getCoef(int dy, int dx) {
+		if (dy < 0) {
+			dx = -dx;
+			dy = -dy;
+		}
+		return coefs[dy][NMAX + dx];
+	}
+
+	void generateCoefs() {
+		const int &N = NMAX;
+		for (int dx = 1; dx < N; ++dx) {
+			for (int dy = 1; dy < N; ++dy) {
+				int commonDiv = gcd(dx, dy);
+				if (commonDiv == 1) {
+					coefs[dy][N + dx] = 1.0 * dy / dx;
+					coefs[dy][N - dx] = -1.0 * dy / dx;
+				} else {
+					int dx2 = dx / commonDiv;
+					int dy2 = dy / commonDiv;
+					coefs[dy][N + dx] = coefs[dy2][N + dx2];
+					coefs[dy][N - dx] = coefs[dy2][N - dx2];
 				}
 			}
 		}
-		if (placementOk) {
-			// cerr << "Placing a queen at pos " << pos << " " << i << endl;
-			queens[pos] = i;
-			if (pos + 1 == N) {
-				printBoard(queens);
-				exit(0);
+	}
+
+	// Initialize the board with random numbers
+	void initializeBoard() {
+		for (int r = 0; r < N; ++r)
+			board[r] = rand() % N;
+	}
+
+	void printSolution() {
+		cout << "===========================================================" << endl;
+		cout << "calculated in " << (float)(clock() - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
+		cout << "echo " << N << endl << "echo ";
+		for (int i = 0; i < N; ++i)
+			cout << board[i] + 1 << " ";
+		cout << endl;
+		/*for (int i=0; i<N; ++i)
+		    cout <<board[i] << ", ";
+		cout <<endl;*/
+		/*
+		for (int r=0; r<N; ++r) {
+		    for (int c=0; c<board[r]; ++c) {
+		        cout <<" ";
+		    }
+		    cout << "#" <<endl;
+		}*/
+		cout << "===========================================================" << endl;
+	}
+
+	int fieldCost(int row, int col, bool halfboard = false) {
+		int maxrow = halfboard ? row : N;
+		int maxazimuths = halfboard ? row : N - 1;
+		int cost = 0;
+		int ur = row + col;
+		int ul = row - col;
+		unordered_set<float> azimuths;
+		for (int k = 0; k < maxrow; ++k) {
+			if (k == row)
+				continue;
+			int qur = k + board[k];
+			int qul = k - board[k];
+			if (qur == ur || qul == ul || board[k] == col)
+				cost += 1;
+			int dy = row - k;
+			int dx = col - board[k];
+			azimuths.insert(getCoef(dy, dx));
+		}
+		cost += (maxazimuths - azimuths.size());
+		return cost;
+	}
+
+	// Find a random row with a cost greater than 0 (meaning their column is wrong)
+	int findNonzerocostRow() {
+		vector<int>* badrows = &buffer;
+		badrows->clear();
+		for (int r = 0; r < N; ++r) {
+			if (fieldCost(r, board[r]) > 0) {
+				badrows->push_back(r);
+			}
+		}
+		if (badrows->empty())
+			return -1;
+		// Get one bad row at random
+		int row = (*badrows)[rand() % badrows->size()];
+		// cout << "fixing row " << row
+		//      << ", bad rows num=" << badrows->size() << endl;
+		return row;
+	}
+
+	// Find the row with the greater cost
+	int findMaxcostRow() {
+		vector<int>* maxrows = &buffer;
+		maxrows->clear();
+		int maxcost = 0;
+		for (int r = 0; r < N; ++r) {
+			int cost = fieldCost(r, board[r]);
+			if (cost > maxcost) {
+				maxrows->clear();
+				maxcost = cost;
+			}
+			if (cost == maxcost) {
+				maxrows->push_back(r);
+			}
+		}
+		if (maxcost == 0)
+			return -1;
+		// Get one bad row at random
+		int row = (*maxrows)[rand() % maxrows->size()];
+		// cout << "fixing row " << row
+		//      << " with cost " << maxcost
+		//      << endl;
+		return row;
+	}
+
+	// Goes over all columns of a specific row and sets its column to the one with the minimum cost.
+	void improveRow(int row) {
+		vector<int>* mincols = &buffer;
+		mincols->clear();
+		// mincost is initialized with the current row score.
+		int mincost = fieldCost(row, board[row]);
+		// For each possible column
+		for (int c = 0; c < N; ++c) {
+			// Skip the column it is already on
+			if (c == board[row])
+				continue;
+			int cost = fieldCost(row, c);
+			if (cost < mincost) {
+				mincols->clear();
+				mincost = cost;
+			}
+			if (cost == mincost) {
+				mincols->push_back(c);
+			}
+		}
+		if (!mincols->empty())
+			board[row] = (*mincols)[rand() % mincols->size()];
+	}
+
+	void search() {
+		int iteration = 0;
+		while (true) {
+			++iteration;
+			// Every 10 iterations, just pick one bad row to randomize a bit
+			int row = iteration % 10 != 0 ? findMaxcostRow() : findNonzerocostRow();
+			if (row < 0) {
+				cout << "Found after " << iteration << " iterations" << endl;
+				return;
+			}
+			if (rand() % ITER_UNTIL_RANDOM == 0) {
+				// to break out of local minimum, randomize a row once in a while
+				// cout << "randomizing row " << row << endl;
+				board[row] = rand() % N;
 			} else {
-				placeQueens(pos + 1, queens);
+				improveRow(row);
 			}
 		}
 	}
-}
 
-int main() {
-	vector<int> queens(N, -1);
+  private:
+	static const int NMAX = 999;
+	static const int ITER_UNTIL_RANDOM = 50;
 
-	cerr << "...................................." << endl;
+	int N;
+	float coefs[NMAX][2 * NMAX];
+	int board[NMAX];
+	time_t startTime;
 
-	placeQueens(0, queens);
 
+	// This vector will have size N and will be reused everytime we need a vector of that size for computations.
+	vector<int> buffer;
+};
+
+int main(int argc, char** argv) {
+	srand(time(0));
+
+	int n = 25;
+
+	if (argc == 2)
+		n = stoi(argv[1]);
+
+	Solver s(n);
 	return 0;
 }
